29cab5c0bce5cbd8b75a09613b6b2d4b63ebff10
Efraim Basilio <basilio.efraim@gmail.com>
2020-01-25 04:26:11 +0800

    Add REpository

---
+++ Taste.Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Taste.Models
+{
+    //always make the model as public
+     public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "DisplayOrderS ")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
---
+++ TasteDataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TasteDataAccess.Data.Repository
+{
+    class UnitOfWork
+    {
+    }
+}
